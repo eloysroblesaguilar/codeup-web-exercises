8eac17fdda79ead52458e93d04cf9c10906bcaf4
Eloys Robles Aguilar <roblesaguilar.eloys@gmail.com>
2021-10-08 10:49:00 -0500

    completed the js with html lec

---
+++ js/js-with-html-lec.js
@@ -0,0 +1,28 @@
+"use strict";
+
+var jeff = 'Jeff';
+console.log(jeff);
+
+console.log("Hello! I'm in my own file!");
+
+var returnOfAlert = alert("TGIF! Thank Goodness I'm Fred!");
+console.log(returnOfAlert);
+
+var returnOfConfirm = confirm("Got any plans for this three day weekend?");
+console.log(returnOfConfirm);
+
+var favoriteNuMetalBand = prompt("What's your favorite nu-metal band?");
+console.log("Ha ha, you like " + favoriteNuMetalBand + "!");
+
+var bothSocksFirst = confirm("Hey , do you put on both socks before putting you shoes on?");
+if (bothSocksFirst) {
+    alert("Yeah, thats's right.");
+} else {
+    prompt("Why?");
+    alert("Weirdo.....");
+}
+
+var leastFavoriteSoda = prompt("What is ytour least favorite soda?");
+var favoriteSoda = prompt("What's your favorite soda?");
+
+alert("Hello fellow teens, my name is " + leastFavoriteSoda+ " " + favoriteSoda + " and I choose hugs, not drugs.");
